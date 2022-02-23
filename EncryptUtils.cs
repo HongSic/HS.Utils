a9c1fc3e24b4076e4cec4e9f6c6eb54121d209a6
HongSic <gpo04174@naver.com>
2022-02-23 11:41:17 +0900

    2022-02-23 More method added in Encrypt/Decrypt

--- EncryptUtils.cs
+++ EncryptUtils.cs
@@ -23,8 +23,13 @@ namespace HS.Utils
         #region Encrypt / Decrypt AES
         //https://kimkitty.net/archives/1520
 
+        private static byte[] UTF8Array(string Text) { return string.IsNullOrEmpty(Text) ? null : Encoding.UTF8.GetBytes(Text); }
+
         #region Encrypt / Decrypt AES Stream
-        public static CryptoStream GetEncryptStreamAES(this Stream BaseStream, string Password, byte[] Salt = null)
+        public static CryptoStream GetEncryptStreamAES(this Stream BaseStream, string Password) { return _GetEncryptStreamAES(BaseStream, Password, null); }
+        public static CryptoStream GetEncryptStreamAES(this Stream BaseStream, string Password, string Salt) { return _GetEncryptStreamAES(BaseStream, Password, UTF8Array(Salt)); }
+        public static CryptoStream GetEncryptStreamAES(this Stream BaseStream, string Password, byte[] Salt) { return _GetEncryptStreamAES(BaseStream, Password, Salt); }
+        private static CryptoStream _GetEncryptStreamAES(this Stream BaseStream, string Password, byte[] Salt)
         {
             RijndaelManaged RijndaelCipher = new RijndaelManaged();
 
@@ -42,7 +47,10 @@ namespace HS.Utils
             return new CryptoStream(BaseStream, Encryptor, CryptoStreamMode.Write);
         }
 
-        public static CryptoStream GetDecryptStreamAES(this Stream BaseStream, string Password, byte[] Salt = null)
+        public static CryptoStream GetDecryptStreamAES(this Stream BaseStream, string Password) { return _GetDecryptStreamAES(BaseStream, Password, null); }
+        public static CryptoStream GetDecryptStreamAES(this Stream BaseStream, string Password, byte[] Salt) { return _GetDecryptStreamAES(BaseStream, Password, Salt); }
+        public static CryptoStream GetDecryptStreamAES(this Stream BaseStream, string Password, string Salt) { return _GetDecryptStreamAES(BaseStream, Password, UTF8Array(Salt)); }
+        private static CryptoStream _GetDecryptStreamAES(this Stream BaseStream, string Password, byte[] Salt = null)
         {
             RijndaelManaged RijndaelCipher = new RijndaelManaged();
 
@@ -58,7 +66,10 @@ namespace HS.Utils
         #endregion
 
         #region Encrypt / Decrypt AES Data
-        public static byte[] EncryptDataAES(this byte[] Data, string Password, byte[] Salt)
+        public static byte[] EncryptDataAES(this byte[] Data, string Password) { return _EncryptDataAES(Data, Password, null); }
+        public static byte[] EncryptDataAES(this byte[] Data, string Password, byte[] Salt) { return _EncryptDataAES(Data, Password, Salt); }
+        public static byte[] EncryptDataAES(this byte[] Data, string Password, string Salt) { return _EncryptDataAES(Data, Password, UTF8Array(Salt)); }
+        private static byte[] _EncryptDataAES(this byte[] Data, string Password, byte[] Salt)
         {
             using (var memory = new MemoryStream())
             using (var crypto = GetEncryptStreamAES(memory, Password, Salt))
@@ -73,47 +84,72 @@ namespace HS.Utils
             }
         }
 
-        public static byte[] DecryptDataAES(this byte[] EncryptedData, string Password, byte[] Salt, out int DecryptedCount)
+        public static byte[] DecryptDataAES(this byte[] EncryptedData, string Password) { return _DecryptDataAES(EncryptedData, Password, null); }
+        public static byte[] DecryptDataAES(this byte[] EncryptedData, string Password, string Salt) { return _DecryptDataAES(EncryptedData, Password, UTF8Array(Salt)); }
+        public static byte[] DecryptDataAES(this byte[] EncryptedData, string Password, byte[] Salt) { return _DecryptDataAES(EncryptedData, Password, Salt); }
+        private static byte[] _DecryptDataAES(this byte[] EncryptedData, string Password, byte[] Salt)
         {
-            using (var memory = new MemoryStream(EncryptedData))
-            using (var crypto = GetDecryptStreamAES(memory, Password, Salt))
+            using (var outmemory = new MemoryStream())
+            using (var inmemory = new MemoryStream(EncryptedData))
+            using (var crypto = GetDecryptStreamAES(inmemory, Password, Salt))
             {
+
                 // 복호화된 데이터를 담을 바이트 배열을 선언한다.
                 // 길이는 알 수 없지만, 일단 복호화되기 전의 데이터의 길이보다는
                 // 길지 않을 것이기 때문에 그 길이로 선언한다.
+
+                /*
                 byte[] buffer = new byte[EncryptedData.Length];
+                DecryptedCount = crypto.Read(buffer, 0, buffer.Length);
+                */
+
+                byte[] buffer = new byte[1024];
 
                 // 복호화 시작
-                DecryptedCount = crypto.Read(buffer, 0, buffer.Length);
+                int DecryptedCount = 0;
+                while ((DecryptedCount = crypto.Read(buffer, 0, buffer.Length)) > 0)
+                    outmemory.Write(buffer, 0, DecryptedCount);
 
-                return buffer;
+                outmemory.Position = 0;
+                return outmemory.ToArray();
             }
         }
         #endregion
 
         #region Encrypt / Decrypt AES String
-        public static string EncryptToBase64AES(this string Text, string Password, string Salt = null) { return Convert.ToBase64String(EncryptStringAES(Text, Password, Salt), Base64FormattingOptions.InsertLineBreaks); }
-        public static byte[] EncryptStringAES(this string Text, string Password, string Salt = null)
+        public static string EncryptToBase64AES(this string Text, string Password) { return Convert.ToBase64String(EncryptStringAES(Text, Password), Base64FormattingOptions.InsertLineBreaks); }
+        public static string EncryptBase64AES(this string Text, string Password, string Salt) { return Convert.ToBase64String(EncryptStringAES(Text, Password, Salt), Base64FormattingOptions.InsertLineBreaks); }
+        public static string EncryptToBase64AES(this string Text, string Password, byte[] Salt) { return Convert.ToBase64String(EncryptStringAES(Text, Password, Salt), Base64FormattingOptions.InsertLineBreaks); }
+
+        private static byte[] EncryptStringAES(this string Text, string Password) { return _EncryptStringAES(Text, Password, null); }
+        private static byte[] EncryptStringAES(this string Text, string Password, string Salt) { return _EncryptStringAES(Text, Password, UTF8Array(Salt)); }
+        private static byte[] EncryptStringAES(this string Text, string Password, byte[] Salt) { return _EncryptStringAES(Text, Password, Salt); }
+        private static byte[] _EncryptStringAES(this string Text, string Password, byte[] Salt)
         {
             // 입력받은 문자열을 바이트 배열로 변환
             byte[] PlainText = Encoding.UTF8.GetBytes(Text);
             // 딕셔너리 공격을 대비해서 키를 더 풀기 어렵게 만들기 위해서 
             // Salt를 사용한다.
-            byte[] salt = string.IsNullOrEmpty(Salt) ? null : Encoding.UTF8.GetBytes(Salt);
-            return EncryptDataAES(PlainText, Password, salt);
+            return EncryptDataAES(PlainText, Password, Salt);
         }
 
 
-        public static string DecryptToBase64AES(this string EncryptedBase64, string Password, string Salt = null) { return DecryptStringAES(Convert.FromBase64String(EncryptedBase64), Password, Salt); }
-        public static string DecryptStringAES(this byte[] EncryptedData, string Password, string Salt = null)
+        public static string DecryptToBase64AES(this string EncryptedBase64, string Password) { return DecryptStringAES(Convert.FromBase64String(EncryptedBase64), Password); }
+        public static string DecryptBase64AES(this string EncryptedBase64, string Password, string Salt = null) { return DecryptStringAES(Convert.FromBase64String(EncryptedBase64), Password, Salt); }
+        public static string DecryptToBase64AES(this string EncryptedBase64, string Password, byte[] Salt = null) { return DecryptStringAES(Convert.FromBase64String(EncryptedBase64), Password, Salt); }
+
+        public static string DecryptStringAES(this byte[] EncryptedData, string Password) { return _DecryptStringAES(EncryptedData, Password, null); }
+        public static string DecryptStringAES(this byte[] EncryptedData, string Password, string Salt) { return _DecryptStringAES(EncryptedData, Password, UTF8Array(Salt)); }
+        public static string DecryptStringAES(this byte[] EncryptedData, string Password, byte[] Salt) { return _DecryptStringAES(EncryptedData, Password, Salt); }
+        private static string _DecryptStringAES(this byte[] EncryptedData, string Password, byte[] Salt)
         {
             // 딕셔너리 공격을 대비해서 키를 더 풀기 어렵게 만들기 위해서 
             // Salt를 사용한다.
-            byte[] salt = string.IsNullOrEmpty(Salt) ? null : Encoding.UTF8.GetBytes(Salt); 
+
             // 복호화 시작
-            var data = DecryptDataAES(EncryptedData, Password, salt, out int DecryptedCount);
+            var data = DecryptDataAES(EncryptedData, Password, Salt);
             // 복호화된 데이터를 문자열로 바꾼다.
-            return Encoding.UTF8.GetString(data, 0, DecryptedCount);
+            return Encoding.UTF8.GetString(data);
         }
         #endregion
         #endregion
